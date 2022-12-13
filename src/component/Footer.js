1d9ca11c19980b125fc450a52fb57a073fb73da8
Asfaquekhan <asfaquekhan15@gmail.com>
2022-12-13 20:14:38 +0530

    fixed_2

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,13 +1,31 @@
 import React from 'react'
-
+import {BsGlobe} from 'react-icons/bs'
 export default function Footer() {
   return (
-    <div className='h-1/3'>
-      <p>Home</p>
-      <p>TV Shows</p>
-      <p>Movies</p>
-      <p>Originals</p>
-      <p>Recently Added</p>
+    <div>
+    <div className='h-80  grid grid-cols-2 p-5 m-5 md:grid-cols-4 py-10 text-slate-400  text-center '>
+     
+      <p>FAQ</p>
+      <p>Investor Relations</p>
+      <p>Privacy</p>
+      <p>Speed Test</p>
+      <p>Help Centre</p>
+      <p>Jobs</p>
+      <p>Cookie Preferences</p>
+      <p>Legal Notices</p>
+      <p>Account</p>
+      <p>Ways to watch</p>
+      <p>Corporate Information</p>
+      <p>Omly on Netflix</p>
+      <p>Media Centre</p>
+      <p>Terms of Use</p>
+      <p>Contact Us</p>
+    </div>
+    <div className='flex justify-between'>
+      
+    <img src="https://media.wired.com/photos/592682057034dc5f91bebab8/master/pass/NetflixLogo2016.jpg" alt="Netflix" width={100} className="mx-auto" />
+    <button className='flex px-4 py-1 items-center'><BsGlobe className='mr-2'/>English</button>
+    </div>
     </div>
   )
 }
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,48 +1,61 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { UserAuth } from "../component/context/Auth";
-
+import { RxDividerVertical } from "react-icons/rx";
 
 export default function MoviePage(props) {
   const [movie, setMovie] = useState();
   const [removie, setRemovie] = useState();
   const { data } = UserAuth();
- 
-  console.log(data)
-  const movieApi =useCallback((id) => {
-    
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/${
-          id ? id : data
-        }?api_key=64ff6c5098cdafbe0dcb9398d4334f83&language=en-US`
-      )
-      .then((res) => setMovie(res.data));
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  },[data])
-  const recomendation = useCallback((id) => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/${
-          id ? id : data
-        }/similar?api_key=64ff6c5098cdafbe0dcb9398d4334f83&language=en-US&page=1`
-      )
-      .then((res) => setRemovie(res.data.results))
-      .catch((error) => console.log(error));
-  },[data])
+
+  console.log(data);
+  const movieApi = useCallback(
+    (id) => {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${
+            id ? id : data
+          }?api_key=64ff6c5098cdafbe0dcb9398d4334f83&language=en-US`
+        )
+        .then((res) => setMovie(res.data));
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+    [data]
+  );
+  const recomendation = useCallback(
+    (id) => {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${
+            id ? id : data
+          }/similar?api_key=64ff6c5098cdafbe0dcb9398d4334f83&language=en-US&page=1`
+        )
+        .then((res) => setRemovie(res.data.results))
+        .catch((error) => console.log(error));
+    },
+    [data]
+  );
 
   useEffect(() => {
     console.log("its from useffect");
     movieApi();
     recomendation();
-   
-  }, [movieApi,recomendation]);
+  }, [movieApi, recomendation]);
+  console.log(movie);
+  const runtime = (num) => {
+    let hours = num / 60;
+    let rhours = Math.floor(hours);
+    let minutes = (hours - rhours) * 60;
+    let rminutes = Math.round(minutes);
+    console.log(rhours);
+
+    return `${rhours}h${rminutes}m`;
+  };
   return (
     <div className="">
-      
       <img
         className=" "
         src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
@@ -51,13 +64,37 @@ export default function MoviePage(props) {
       <div className="md:absolute md:top-[11.5%]  md:w-1/2 md:h-[770px]  bg-gradient-to-r from-black  ">
         <div className="md:py-20 p-5 md;space-y-8 space-y-5">
           <h2 className="font-bold text-2xl  leading-8">{movie?.title}</h2>
-          <div className="flex space-x-6 text-slate-400 font-bold">
-            <p>{movie?.release_date.slice(0, 4)}</p>
+          <div className="flex items-center  text-slate-400 font-bold space-x-2">
+            <p className="">{movie?.release_date.slice(0, 4)}</p>
+            <p className="m-2">
+              <RxDividerVertical />
+            </p>
+            <p>{runtime(movie?.runtime)}</p>
+          </div>
+          <div className="flex  text-slate-400 font-bold items-center overflow-clip">
+          {(movie?.adult)?(<p>18+</p>):(<p>NA</p>)}
             {movie?.genres?.map((curr, id) => {
-              return <p key={id}>{curr.name}</p>;
+              return (
+                <div className="flex items-center">
+                 
+                 
+                  <p className="m-2">
+                    <RxDividerVertical />
+                  </p>
+                  <p key={id}>{curr.name}</p>
+                </div>
+              );
             })}
           </div>
+          
           <p className="w-full md:w-4/5">{movie?.overview}</p>
+          <div className="flex  text-slate-400 font-bold items-center space-x-3" >
+            {movie?.spoken_languages.map((curr)=>{
+              return <div>
+                <p>{curr.english_name}</p>
+              </div>
+            })}
+          </div>
         </div>
       </div>
       <h2 className="text-2xl text-center py-5">More Like This</h2>
@@ -72,7 +109,6 @@ export default function MoviePage(props) {
               onClick={() => {
                 movieApi(curr.id);
                 recomendation(curr.id);
-               
               }}
             />
           );
